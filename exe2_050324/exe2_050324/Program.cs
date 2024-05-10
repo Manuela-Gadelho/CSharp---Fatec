4a3b440374db582755e21e23cd9cd76cafe445f4
Manuela Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2024-05-10 10:53:01 -0300

    Add files via upload

---
+++ exe2_050324/exe2_050324/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int contador = 0;
+        int soma = 0;
+
+        Console.WriteLine("Digite números positivos!");
+
+        while (true)
+        {
+            Console.Write("Digite um número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero < 0)
+            {
+                break;
+            }
+
+            soma += numero;
+            contador++;
+        }
+
+        if (contador > 0)
+        {
+            double media = (double)soma / contador;
+            Console.WriteLine($"A média dos números digitados é: {media}");
+        }
+        else
+        {
+            Console.WriteLine("Nenhum número positivo foi digitado.");
+        }
+    }
+}
---
+++ exe2_050324/exe3_050324/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int contador = 0;
+        Console.WriteLine("Digite 0 (zero) para parar");
+
+        while (true)
+        {
+            Console.Write("Digite um número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero == 0)
+            {
+                break;
+            }
+
+            if (numero >= 100 && numero <= 200)
+            {
+                contador ++;
+            }
+        }
+
+        Console.WriteLine($"Foram digitados {contador} números entre 100 e 200.");
+    }
+}
